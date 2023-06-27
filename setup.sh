d9219c229397f891bf53e6f4f23b826d7ba65a98
wrongbaud <wrongbaud@gmail.com>
2023-06-27 16:08:11 -0400

    Adding updated setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 sudo apt-get update && sudo apt-get upgrade
 
-sudo apt-get install vim tmux git build-essential kpartx pulseview ghex bless ipython3 python3-pip emacs chromium-browser filezilla screen binwalk neovim wireshark gparted minicom lzma-alone unzip flashrom can-utils hexedit sigrok-firmware-fx2lafw gh openjdk-19-jdk curl jq docker npm gdb-multiarch libfuse-dev dfu-util libglfw3 libmbedtls14 net-tools uftpd
+sudo apt-get install vim tmux git build-essential kpartx pulseview ghex bless ipython3 python3-pip emacs chromium-browser filezilla screen binwalk neovim wireshark gparted minicom lzma-alone unzip flashrom can-utils hexedit sigrok-firmware-fx2lafw gh openjdk-19-jdk curl jq docker npm gdb-multiarch libfuse-dev dfu-util libglfw3 libmbedtls14 net-tools libglfw3 libmbedcrypto7 libmbedtls14 libmbedx509-1 feh
+
 
 sudo snap install --classic code
 
@@ -33,6 +34,10 @@ done
 # Unzip and remove archive
 unzip ghidra*.zip
 rm *.zip
+
+wget https://api.github.com/repos/NationalSecurityAgency/ghidra/zipball/Ghidra_10.3.1_build
+
+unzip Ghidra_10.3.1_build
 
 # Download Logic
 wget -O Logic.appimage https://logic2api.saleae.com/download?os=linux 
